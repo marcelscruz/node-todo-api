64fc8c37960f7fa6349569795c2bbedce2eeb127
Marcel Cruz <marcelcruz@live.com>
2018-08-12 22:42:25 +0100

    setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 // not needed for mongoose v5+
 // mongoose.Promise = global.Promise
 mongoose.connect(
-  'mongodb://localhost:27017/TodosApp',
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/TodosApp',
   { useNewUrlParser: true },
 )
 
