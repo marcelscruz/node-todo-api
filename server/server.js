9dbc822c465edafb1ee19de2208a50e845a84b83
Marcel Cruz <marcelcruz@live.com>
2018-08-12 21:31:00 +0100

    add tests for GET /todos

--- server/server.js
+++ server/server.js
@@ -25,14 +25,13 @@ app.post('/todos', (req, res) => {
 })
 
 app.get('/todos', (req, res) => {
-  Todo.find().then(
-    todos => {
+  Todo.find()
+    .then(todos => {
       res.send({ todos })
-    },
-    e => {
+    })
+    .catch(e => {
       res.status(400).send(e)
-    },
-  )
+    })
 })
 
 app.listen(3000, () => {
