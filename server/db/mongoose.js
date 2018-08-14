46c22e4c3695ea8e4c2a96ecadf8387a7f64fd4c
Marcel Cruz <marcelcruz@live.com>
2018-08-14 15:27:43 +0100

    hashing added to playground

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,9 +2,14 @@ const mongoose = require('mongoose')
 
 // not needed for mongoose v5+
 // mongoose.Promise = global.Promise
-mongoose.connect(
-  process.env.MONGODB_URI,
-  { useNewUrlParser: true },
-)
+console.log(process.env.MONGODB_URI)
+mongoose
+  .connect(
+    process.env.MONGODB_URI,
+    { useNewUrlParser: true },
+  )
+  .catch(e => {
+    console.log(e)
+  })
 
 module.exports = { mongoose }
--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 const User = mongoose.model('User', {
   email: {
@@ -6,7 +7,29 @@ const User = mongoose.model('User', {
     required: true,
     minlength: 1,
     trim: true,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: '{VALUE} is not a valid email',
+    },
   },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+  tokens: [
+    {
+      access: {
+        type: String,
+        required: true,
+      },
+      token: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
 })
 
 module.exports = { User }
