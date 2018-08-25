6ef0659d46027ac4b9baa4893d5b34b163d03484
Marcel Cruz <marcelcruz@live.com>
2018-08-25 22:30:02 +0100

    add GET /users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -55,6 +55,27 @@ UserSchema.methods.generateAuthToken = function() {
   })
 }
 
+UserSchema.statics.findByToken = function(token) {
+  const User = this
+  let decoded
+
+  try {
+    decoded = jwt.verify(token, 'abc123')
+  } catch (e) {
+    // return new Promise((resolve, reject) => {
+    //   reject()
+    // })
+    // Short version below
+    return Promise.reject()
+  }
+
+  return User.findOne({
+    _id: decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth',
+  })
+}
+
 const User = mongoose.model('User', UserSchema)
 
 module.exports = { User }
