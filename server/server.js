125f9f80169c783f1dd847c4a77115978c0cc26c
Marcel Cruz <marcelcruz@live.com>
2018-08-27 20:27:44 +0100

    add tests for DELETE /users/me/token

--- server/server.js
+++ server/server.js
@@ -155,6 +155,7 @@ app.post('/users/login', (req, res) => {
     })
 })
 
+// DELETE /users/me/token
 app.delete('/users/me/token', authenticate, (req, res) => {
   req.user
     .removeToken(req.token)
